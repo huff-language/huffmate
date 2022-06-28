5842ac2ec3c5ec90c914363c1251ad1fc266ab9b
Andreas Bigger <abigger87@gmail.com>
2022-06-28 14:12:23 -0400

    auth.sol

--- test/auth/Auth.t.sol
+++ test/auth/Auth.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.13;
+pragma solidity ^0.8.15;
 
 // import "forge-std/console.sol";
 
@@ -10,23 +10,76 @@ interface Auth {
   function setOwner(address) external;
   function setAuthority(address) external;
   function owner() external returns (address);
+  function authority() external returns (address);
 }
 
 contract HuffDeployerTest is Test {
     Auth auth;
 
+    address constant OWNER = address(0x420);
+    address constant INIT_AUTHORITY = address(0x0);
+
+    event OwnerUpdated(address indexed user, address indexed newOwner);
+    event AuthorityUpdated(address indexed user, address indexed newAuthority);
+
     function setUp() public {
-      auth = Auth(HuffDeployer.deploy("auth/Auth"));
+      bytes memory owner = abi.encode(OWNER);
+      bytes memory authority = abi.encode(INIT_AUTHORITY);
+
+      // Create Constructor
+      vm.expectEmit(true, true, true, true);
+      emit AuthorityUpdated(address(this), INIT_AUTHORITY);
+      emit OwnerUpdated(address(this), OWNER);
+      auth = Auth(HuffDeployer.deploy_with_args(
+          "auth/Auth",
+          bytes.concat(owner, authority)
+      ));
     }
 
+    /// OWNER TESTS
+
     function testGetOwner() public {
-      address owner = auth.owner();
-      // assertEq(address(this), auth.owner());
+      assertEq(OWNER, auth.owner());
     }
 
     function testSetOwner(address owner) public {
-      if (owner == address(0)) vm.expectRevert();
+      if (owner == OWNER) return;
+      vm.startPrank(owner);
+      vm.expectRevert();
       auth.setOwner(owner);
-      // assertEq(owner, auth.OWNER());
+      vm.stopPrank();
+      assertEq(OWNER, auth.owner());
+    }
+
+    function testOwnerCanSetOwner() public {
+      address new_owner = address(0x50ca1);
+      vm.startPrank(OWNER);
+      auth.setOwner(new_owner);
+      vm.stopPrank();
+      assertEq(new_owner, auth.owner());
+    }
+
+    /// AUTHORITY TESTS
+
+    function testAuthority() public {
+      assertEq(INIT_AUTHORITY, auth.authority());
+    }
+
+    function testSetAuthority(address owner) public {
+      address new_authority = address(0x50ca1);
+      if (owner == OWNER) return;
+      vm.startPrank(owner);
+      vm.expectRevert();
+      auth.setAuthority(new_authority);
+      vm.stopPrank();
+      assertEq(INIT_AUTHORITY, auth.authority());
+    }
+
+    function testOwnerCanSetAuthority() public {
+      address new_authority = address(0x50ca1);
+      vm.startPrank(OWNER);
+      auth.setAuthority(new_authority);
+      vm.stopPrank();
+      assertEq(new_authority, auth.authority());
     }
 }
