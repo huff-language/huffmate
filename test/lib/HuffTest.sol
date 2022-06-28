1570723050820de3d17b9817018824ca92dd96b8
clabby <ben@clab.by>
2022-06-28 09:33:47 -0400

    Add tests with `foundry-huff`

--- test/lib/HuffTest.sol
+++ test/lib/HuffTest.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.10;
+pragma solidity ^0.8.15;
 
 import {Test} from "forge-std/Test.sol";
 import {HuffDeployer} from "foundry-huff/HuffDeployer.sol";
@@ -15,6 +15,6 @@ contract HuffTest is Test {
 
     /// @dev Deploy a new contract.
     function deploy() internal returns (address) {
-        return new HuffDeployer().deploy(name);
+        return HuffDeployer.deploy(name);
     }
 }
