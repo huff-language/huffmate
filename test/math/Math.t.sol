b6277b364ab409e1090f18d980acc6c2e25059e6
kadenzipfel <kaden.zipfel@hotmail.com>
2022-07-05 15:55:18 -0700

    Fix sqrt

--- test/math/Math.t.sol
+++ test/math/Math.t.sol
@@ -19,12 +19,62 @@ contract MathTest is Test {
         math = Math(HuffDeployer.deploy("math/Math"));
     }
 
-    function sqrt(uint x) public pure returns (uint y) {
-        uint z = (x + 1) / 2;
-        y = x;
-        while (z < y) {
-            y = z;
-            z = (x / z + z) / 2;
+    // Source: https://github.com/OpenZeppelin/openzeppelin-contracts/blob/e7397844f8dd9b54fb4227b91b20f3bd2e82dab2/contracts/utils/math/Math.sol#L158
+    function sqrt(uint256 a) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+
+        // For our first guess, we get the biggest power of 2 which is smaller than the square root of the target.
+        // We know that the "msb" (most significant bit) of our target number `a` is a power of 2 such that we have
+        // `msb(a) <= a < 2*msb(a)`.
+        // We also know that `k`, the position of the most significant bit, is such that `msb(a) = 2**k`.
+        // This gives `2**k < a <= 2**(k+1)` → `2**(k/2) <= sqrt(a) < 2 ** (k/2+1)`.
+        // Using an algorithm similar to the msb computation, we are able to compute `result = 2**(k/2)` which is a
+        // good first approximation of `sqrt(a)` with at least 1 correct bit.
+        uint256 result = 1;
+        uint256 x = a;
+        if (x >> 128 > 0) {
+            x >>= 128;
+            result <<= 64;
+        }
+        if (x >> 64 > 0) {
+            x >>= 64;
+            result <<= 32;
+        }
+        if (x >> 32 > 0) {
+            x >>= 32;
+            result <<= 16;
+        }
+        if (x >> 16 > 0) {
+            x >>= 16;
+            result <<= 8;
+        }
+        if (x >> 8 > 0) {
+            x >>= 8;
+            result <<= 4;
+        }
+        if (x >> 4 > 0) {
+            x >>= 4;
+            result <<= 2;
+        }
+        if (x >> 2 > 0) {
+            result <<= 1;
+        }
+
+        // At this point `result` is an estimation with one bit of precision. We know the true value is a uint128,
+        // since it is the square root of a uint256. Newton's method converges quadratically (precision doubles at
+        // every iteration). We thus need at most 7 iteration to turn our partial result with one bit of precision
+        // into the expected uint128 result.
+        unchecked {
+            result = (result + a / result) >> 1;
+            result = (result + a / result) >> 1;
+            result = (result + a / result) >> 1;
+            result = (result + a / result) >> 1;
+            result = (result + a / result) >> 1;
+            result = (result + a / result) >> 1;
+            result = (result + a / result) >> 1;
+            return result < a / result ? result : a / result;
         }
     }
 
@@ -33,10 +83,10 @@ contract MathTest is Test {
        assertEq(result, 8);
     }
 
-    // function testSqrt(uint16 num) public {
-    //     uint256 result = math.sqrt(num);
-    //     assertEq(result, sqrt(num));
-    // }
+    function testSqrt(uint256 num) public {
+        uint256 result = math.sqrt(num);
+        assertEq(result, sqrt(num));
+    }
 
     function testMax() public {
         uint256 result = math.max(420, 69);
