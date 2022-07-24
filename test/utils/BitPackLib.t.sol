f9bb751b93888f9f0f0cb69804a078ef0329baca
kadenzipfel <kaden.zipfel@hotmail.com>
2022-07-24 15:29:33 -0700

    Fix UNPACK_FROM_LEFT

--- test/utils/BitPackLib.t.sol
+++ test/utils/BitPackLib.t.sol
@@ -7,6 +7,7 @@ import "forge-std/Test.sol";
 interface IBitPackLib {
     function packValue(bytes32, uint256, uint256, uint256) external pure returns (bytes32);
     function unpackValueFromRight(bytes32, uint256) external pure returns (uint256);
+    function unpackValueFromLeft(bytes32, uint256) external pure returns (uint256);
 }
 
 contract BitPackLibTest is Test {
@@ -27,6 +28,12 @@ contract BitPackLibTest is Test {
     function testUnpackValueFromRight() public {
         bytes32 newWord = bitPackLib.packValue(bytes32(0), 0x696969, 232, 24);
         uint256 value = bitPackLib.unpackValueFromRight(newWord, 24);
+        assertEq(value, 0x696969);
+    }
+
+    function testUnpackValueFromLeft() public {
+        bytes32 newWord = bitPackLib.packValue(bytes32(0), 0x696969, 0, 24);
+        uint256 value = bitPackLib.unpackValueFromLeft(newWord, 24);
         assertEq(value, 0x696969);
     }
 }
