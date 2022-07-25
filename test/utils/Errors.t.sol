2ed2506a137bc7f33794357ba51eade12b74de99
jtriley <jtriley15@gmail.com>
2022-07-25 09:15:19 -0400

    update tests

--- test/utils/Errors.t.sol
+++ test/utils/Errors.t.sol
@@ -20,6 +20,17 @@ interface IErrorsMock {
     function simulateBubbleUpIfFailed(address) external view;
 }
 
+uint256 constant COMPILER_PANIC = 0x00;
+uint256 constant ASSERT_FALSE = 0x01;
+uint256 constant ARITHMETIC_OVERFLOW = 0x11;
+uint256 constant DIVIDE_BY_ZERO = 0x12;
+uint256 constant INVALID_ENUM_VALUE = 0x21;
+uint256 constant INVALID_STORAGE_BYTE_ARRAY = 0x22;
+uint256 constant EMPTY_ARRAY_POP = 0x31;
+uint256 constant ARRAY_OUT_OF_BOUNDS = 0x32;
+uint256 constant MEMORY_TOO_LARGE = 0x41;
+uint256 constant UNINITIALIZED_FUNCTION_POINTER = 0x51;
+
 // Utils
 bytes4 constant errorSelector = 0x08c379a0;
 bytes4 constant panicSelector = 0x4e487b71;
@@ -56,7 +67,79 @@ contract ErrorsTest is Test {
         try eLib.simulateAssert() {
             revert("did not fail");
         } catch Panic(uint256 panicCode) {
-            assertEq(panicCode, 1);
+            assertEq(panicCode, ASSERT_FALSE);
+        }
+    }
+
+    function testCompilerPanic() public {
+        try eLib.simulateCompilerPanic() {
+            revert("did not fail");
+        } catch Panic(uint256 panicCode) {
+            assertEq(panicCode, COMPILER_PANIC);
+        }
+    }
+
+    function testArithmeticOverflow() public {
+        try eLib.simulateArithmeticOverflow() {
+            revert("did not fail");
+        } catch Panic(uint256 panicCode) {
+            assertEq(panicCode, ARITHMETIC_OVERFLOW);
+        }
+    }
+
+    function testDivideByZero() public {
+        try eLib.simulateDivideByZero() {
+            revert("did not fail");
+        } catch Panic(uint256 panicCode) {
+            assertEq(panicCode, DIVIDE_BY_ZERO);
+        }
+    }
+
+    function testInvalidEnumValue() public {
+        try eLib.simulateInvalidEnumValue() {
+            revert("did not fail");
+        } catch Panic(uint256 panicCode) {
+            assertEq(panicCode, INVALID_ENUM_VALUE);
+        }
+    }
+
+    function testInvalidStorageByteArray() public {
+        try eLib.simulateInvalidStorageByteArray() {
+            revert("did not fail");
+        } catch Panic(uint256 panicCode) {
+            assertEq(panicCode, INVALID_STORAGE_BYTE_ARRAY);
+        }
+    }
+
+    function testEmptyArrayPop() public {
+        try eLib.simulateEmptyArrayPop() {
+            revert("did not fail");
+        } catch Panic(uint256 panicCode) {
+            assertEq(panicCode, EMPTY_ARRAY_POP);
+        }
+    }
+
+    function testArrayOutOfBounds() public {
+        try eLib.simulateArrayOutOfBounds() {
+            revert("did not fail");
+        } catch Panic(uint256 panicCode) {
+            assertEq(panicCode, ARRAY_OUT_OF_BOUNDS);
+        }
+    }
+
+    function testMemoryTooLarge() public {
+        try eLib.simulateMemoryTooLarge() {
+            revert("did not fail");
+        } catch Panic(uint256 panicCode) {
+            assertEq(panicCode, MEMORY_TOO_LARGE);
+        }
+    }
+
+    function testUninitializedFunctionPointer() public {
+        try eLib.simulateUninitializedFunctionPointer() {
+            revert("did not revert");
+        } catch Panic(uint256 panicCode) {
+            assertEq(panicCode, UNINITIALIZED_FUNCTION_POINTER);
         }
     }
 }
