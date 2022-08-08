e2d71b3c1da2c5c6594eebcd03cd8a154836a841
iFrostizz <franfran@DESKTOP-T7KAS9N.localdomain>
2022-08-08 14:13:21 +0200

    starting Calls

---
+++ test/utils/Calls.t.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+import "forge-std/Test.sol";
+import "foundry-huff/HuffDeployer.sol";
+
+interface ICalls {
+    // function simpleCall(address, uint256, uint256, uint256, uint256) external payable returns (bool, bytes memory);
+    function simpleCall(address) external payable returns (bool);
+
+    function delegateCall(address, uint256, uint256, uint256, uint256) external payable returns (bool, bytes memory);
+
+    function tst(uint256) external;
+
+    function call(address) external payable returns (bool);
+}
+
+contract Dummy is Test {
+    bool public triggered;
+    uint256 public withVal;
+
+    function get(uint256 val) external pure returns (uint256) {
+        return val;
+    }
+
+    fallback() external payable {
+        emit log("1");
+        // triggered = true;
+        emit log("2");
+        // withVal = msg.value;
+        emit log("3");
+    }
+}
+
+contract CallsTest is Test {
+    ICalls calls;
+    Dummy dummy;
+
+    function setUp() public {
+        calls = ICalls(HuffDeployer.deploy("utils/Calls"));
+        dummy = new Dummy();
+    }
+
+    function testSimpleCall() public {
+        // calls.simpleCall(address(dummy), 0, 0, 0, 0);
+        bool success = calls.simpleCall(address(dummy));
+        assertTrue(success);
+        assertTrue(dummy.triggered());
+        assertEq(dummy.withVal(), 0);
+    }
+}
