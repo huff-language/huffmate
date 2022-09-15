8097a1770e2947202ac4f5bc73718f606f538271
Andreas Bigger <abigger87@gmail.com>
2022-09-15 13:02:53 -0400

    work has been done, progress made

--- test/tokens/ERC4626.t.sol
+++ test/tokens/ERC4626.t.sol
@@ -35,8 +35,8 @@ interface IERC4626 is IERC20 {
     function asset() external view returns (address);
 
     function deposit(uint256 assets, address receiver) external returns (uint256 shares);
-    function mint(uint256 shares, address receiver) external returns (uint256 assets);
     function withdraw(uint256 assets, address receiver, address owner) external returns (uint256 shares);
+    function mint(uint256 shares, address receiver) external returns (uint256 assets);
     function redeem(uint256 shares, address receiver, address owner) external returns (uint256 assets);
 
     function totalAssets() external view returns (uint256);
