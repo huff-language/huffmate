4bd600986245a2fb37d461dda213991709fb4f7b
Andreas Bigger <abigger87@gmail.com>
2022-09-19 11:06:23 -0400

    sstore2 worksssss :rocket:

--- test/utils/SSTORE2.t.sol
+++ test/utils/SSTORE2.t.sol
@@ -5,9 +5,9 @@ import "foundry-huff/HuffDeployer.sol";
 import "forge-std/Test.sol";
 
 interface ISSTORE2 {
-    function read(address, uint256) external view returns (bytes memory);
-    function read(address, uint256, uint256) external view returns (bytes memory);
-    function read(address) external view returns (bytes memory);
+    function read(address, uint256) external returns (bytes memory);
+    function read(address, uint256, uint256) external returns (bytes memory);
+    function read(address) external returns (bytes memory);
     function write(bytes memory) external returns (address);
 }
 
@@ -19,9 +19,49 @@ contract SSTORE2Test is Test {
         store = ISSTORE2(HuffDeployer.deploy_with_code("utils/SSTORE2", wrapper_code));
     }
 
-    /// @notice Test writing to storage
-    function testWriting() public {
-        store.write(bytes("hello world"));
+    /// @notice Test writing and reading from storage with sstore2
+    function testSmallWriteRead() public {
+        bytes memory input = bytes("hello world");
+        bytes memory expectedCodePacked = abi.encodePacked(
+            hex"00",
+            input
+        );
+
+        // Store "hello world"
+        address pointer = store.write(input);
+        assert(pointer != address(0));
+        assertEq(expectedCodePacked, pointer.code);
+
+        // Try to read back from the pointer
+        bytes memory result = store.read(pointer);
+        bytes memory packedResult = abi.encodePacked(
+            hex"00",
+            result
+        );
+        assertEq(packedResult, expectedCodePacked);
+        assertEq(packedResult, pointer.code);
     }
 
+    /// @notice Test writing and reading from storage with sstore2
+    function testMultiWordWriteRead() public {
+        bytes memory input = bytes("this string is longer than 32 bytes and will require multiple words to store");
+        bytes memory expectedCodePacked = abi.encodePacked(
+            hex"00",
+            input
+        );
+
+        // Store "hello world"
+        address pointer = store.write(input);
+        assert(pointer != address(0));
+        assertEq(expectedCodePacked, pointer.code);
+
+        // Try to read back from the pointer
+        bytes memory result = store.read(pointer);
+        bytes memory packedResult = abi.encodePacked(
+            hex"00",
+            result
+        );
+        assertEq(packedResult, expectedCodePacked);
+        assertEq(packedResult, pointer.code);
+    }
 }
