7092f23bb0a2cb10ce46994238b5715ef9642934
franfran <fgg67@hotmail.fr>
2022-10-06 09:30:36 +0200

    no need to modulo

--- test/data-structures/Bytes.t.sol
+++ test/data-structures/Bytes.t.sol
@@ -11,8 +11,10 @@ interface IBytes {
     function concatMemoryAndSet3() external;
     function concatMemoryAndSet4() external;
     function concatMemoryAndSet5() external;
+    function concatMemoryAndSet6() external;
     function sliceMemoryAndSet1() external;
     function sliceMemoryAndSet2() external;
+    function sliceMemoryAndSet3() external;
 }
 
 contract BytesTest is Test {
@@ -68,6 +70,14 @@ contract BytesTest is Test {
         assertEq(vm.load(address(b), bytes32(uint256(64))), bytes32(0));
     }
     
+    function testConcat6() public {
+        b.concatMemoryAndSet6();
+        
+        assertEq(vm.load(address(b), bytes32(0)), bytes32(uint256(15)));
+        assertEq(vm.load(address(b), bytes32(uint256(32))), bytes32(bytes15(0xbabe1babe1babe1babe1babe2babe2)));
+        assertEq(vm.load(address(b), bytes32(uint256(64))), bytes32(0));
+    }
+    
     function testSlice1() public {
         b.sliceMemoryAndSet1();
         assertEq(vm.load(address(b), bytes32(0)), bytes32(uint256(16)));
@@ -79,5 +89,11 @@ contract BytesTest is Test {
         assertEq(vm.load(address(b), bytes32(0)), bytes32(uint256(36)));
         assertEq(vm.load(address(b), bytes32(uint256(32))), bytes32(0x1babe1babe1babe1babe1babe1babe1babe1babe1babe1babe1babe1babebabe));
         assertEq(vm.load(address(b), bytes32(uint256(64))), bytes32(bytes4(0x2babe2ba)));
+    }
+    
+    function testSlice3() public {
+        b.sliceMemoryAndSet3();
+        assertEq(vm.load(address(b), bytes32(0)), bytes32(uint256(4)));
+        assertEq(vm.load(address(b), bytes32(uint256(32))), bytes32(bytes4(0xbabe2bab)));
     }
 }
